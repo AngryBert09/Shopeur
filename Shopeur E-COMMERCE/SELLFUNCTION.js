de1f622878972653a830829cbc544487e7513133
Angry Bert <104261295+AngryBert09@users.noreply.github.com>
2022-12-02 14:28:36 +0800

    UI CHANGES ADD SOME LOADING ANIMATIONS

---
+++ Shopeur E-COMMERCE/SELLFUNCTION.js
@@ -0,0 +1,6 @@
+document.addEventListener("click", addtoCart);
+
+function addtoCart() {
+  document.getElementById("1st item").style.color = "yellow";
+  document.getElementById("cart").src = "images/Sell Shop Images/cart1item.png";
+}

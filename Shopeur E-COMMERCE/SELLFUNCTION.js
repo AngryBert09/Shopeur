21721f62ee7ed5be749f792645a3403e8a5bc300
Angry Bert <104261295+AngryBert09@users.noreply.github.com>
2022-12-14 12:26:43 +0800

    Quick Fix Hyperlinks

--- Shopeur E-COMMERCE/SELLFUNCTION.js
+++ Shopeur E-COMMERCE/SELLFUNCTION.js
@@ -1,6 +1,7 @@
 document.addEventListener("click", addtoCart);
 
 function addtoCart() {
-  document.getElementById("1st item").style.color = "yellow";
-  document.getElementById("cart").src = "images/Sell Shop Images/cart1item.png";
+  var item = (document.getElementById("1st item").style.color = "yellow");
+  var cart = (document.getElementById("cart").src =
+    "images/Sell Shop Images/cart1item.png");
 }
